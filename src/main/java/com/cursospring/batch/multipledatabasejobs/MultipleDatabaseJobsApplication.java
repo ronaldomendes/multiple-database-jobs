a49624ea8899663dda7cbaa535d79351315fa7f6
Ronaldo Mendes <mendes-ronaldo@live.com>
2022-03-04 23:04:48 -0300

    Creating project

---
+++ src/main/java/com/cursospring/batch/multipledatabasejobs/MultipleDatabaseJobsApplication.java
@@ -0,0 +1,13 @@
+package com.cursospring.batch.multipledatabasejobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatabaseJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDatabaseJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursospring/batch/multipledatabasejobs/MultipleDatabaseJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.batch.multipledatabasejobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDatabaseJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
