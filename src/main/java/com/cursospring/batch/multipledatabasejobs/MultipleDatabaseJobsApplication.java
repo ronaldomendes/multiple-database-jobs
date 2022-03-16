a3408ccb302465d57cf230c4f8d428bdac296e89
Ronaldo Mendes <mendes-ronaldo@live.com>
2022-03-16 19:21:32 -0300

    Editing database configs

--- src/main/java/com/cursospring/batch/multipledatabasejobs/MultipleDatabaseJobsApplication.java
+++ src/main/java/com/cursospring/batch/multipledatabasejobs/MultipleDatabaseJobsApplication.java
@@ -1,9 +1,11 @@
 package com.cursospring.batch.multipledatabasejobs;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class MultipleDatabaseJobsApplication {
 
 	public static void main(String[] args) {
